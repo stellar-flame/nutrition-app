047575e7835c308842e9a8e1d5c9d6bd84a7196a
hava <hava.nagdi@gmail.com>
2025-07-31 18:03:51 -0700

    Fix baseURL typo in axios configuration

--- frontend/api/axios.ts
+++ frontend/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rwcrl3bz8.execute-api.us-east-1.amazonaws.com/prod',
+  baseURL: 'https://rwcr3l3bz8.execute-api.us-east-1.amazonaws.com/prod',
   // baseURL: 'http://192.168.0.199:8000',
   headers: {
     'Content-Type': 'application/json',
